4de4aed27ccc2e26a5f413605a6b5b2dfb23103a
Jorge Durango <djorge.ivan97@gmail.com>
2019-04-10 14:23:22 -0500

    resolved pending migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_191608) do
+ActiveRecord::Schema.define(version: 2019_04_10_191829) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
