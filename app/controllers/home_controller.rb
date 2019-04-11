8c9728055415467428f53e68b22f8b2d93ac3364
Jorge Durango <djorge.ivan97@gmail.com>
2019-04-11 18:39:31 -0500

    menu design and desc order for posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.order("created_at DESC").limit(3)
     @first_article = Article.last
   end
 end
