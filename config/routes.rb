e70ad7854110ed2753b66516b156a5b371aa6b8f
Jorge Durango <djorge.ivan97@gmail.com>
2019-04-11 19:20:41 -0500

    articles controller and index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  resources :articles
   resources :about
 end
